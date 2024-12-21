3b3fcdf581c26a853b8176bf674dd3d70f9eb5f0
Ben <benogega@gmail.com>
2024-12-21 16:13:20 +0300

    Add hello.cpp project

---
+++ hell-world/hello.cpp
@@ -0,0 +1,17 @@
+/*
+First project in C++. A series of projects aimed at learning C++
+Author: Benard Ogega
+Date: 21/12/2024
+Version: v0.1
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
